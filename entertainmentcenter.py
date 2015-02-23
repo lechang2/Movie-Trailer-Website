c4c0a4cad55dd3b77d302a56310922e059e29242
Le Chang <lechang2@gmail.com>
2015-02-22 22:34:03 -0800

    Movie Trailer Website
    The class project of Udacity full stack web development

---
+++ entertainmentcenter.py
@@ -0,0 +1,35 @@
+import media
+import fresh_tomatoes
+
+gladiator_storyline='The general who becomes a slave, the slave who becomes a gladiator, the gladiator who defines an empiror.'
+gladiator_youtube='https://www.youtube.com/watch?v=ol67qo3WhJk'
+gladiator_poster='http://upload.wikimedia.org/wikipedia/en/8/8d/Gladiator_ver1.jpg'
+gladiator=media.Movie('Gladiator',gladiator_storyline,gladiator_poster,gladiator_youtube)
+
+incredibles_storyline='A family of superheros gets on a mission to beat a science villan.'
+incredibles_youtube='https://www.youtube.com/watch?v=1LASc8ewLaw'
+incredibles_poster='http://img3.wikia.nocookie.net/__cb20100622151355/pixar/images/4/44/Movie_poster_the_incredibles.jpg'
+incredibles=media.Movie('The Incredibles',incredibles_storyline,incredibles_poster,incredibles_youtube)
+
+johnwick_storyline='The retired killer gets his revenge against the ones that hurt his loved ones.'
+johnwick_youtube='https://www.youtube.com/watch?v=2AUmvWm5ZDQ'
+johnwick_poster='http://i.imgur.com/nFtglna.jpg'
+johnwick=media.Movie('John Wick',johnwick_storyline,johnwick_poster,johnwick_youtube)
+
+family_storyline='A retired mafia leader and his family move to a small French town, where things become interesting'
+family_youtube='https://www.youtube.com/watch?v=nwZNypYmPFE'
+family_poster='http://upload.wikimedia.org/wikipedia/en/7/75/The_Family_2013%2C_Poster.jpg'
+family=media.Movie('The Family',family_storyline,family_poster,family_youtube)
+
+interstellar_storyline='Human is near distinction, and NASA is sending astronauts through a worm-hole to find new planets to live in.'
+interstellar_youtube='https://www.youtube.com/watch?v=0vxOhd4qlnA'
+interstellar_poster='http://img2.wikia.nocookie.net/__cb20141003183252/interstellarfilm/images/b/bd/Interstellar-2014-Movie-Poster.jpg'
+interstellar=media.Movie('Interstellar',interstellar_storyline,interstellar_poster,interstellar_youtube)
+
+porco_rosso_storyline='The famous pilot, Porco Rosso, who turned into a red pig by a curse, getts challenged by an American pilot for his glory and love.'
+porco_rosso_youtube='https://www.youtube.com/watch?v=awEC-aLDzjs'
+porco_rosso_poster='http://images.moviepostershop.com/porco-rosso-movie-poster-1992-1020670123.jpg'
+porco_rosso=media.Movie('Porco Rosso', porco_rosso_storyline,porco_rosso_poster,porco_rosso_youtube)
+
+movies=[gladiator,incredibles,johnwick,family,porco_rosso,interstellar]
+
